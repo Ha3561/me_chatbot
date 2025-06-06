10d4f74b1cbcac12a0cb14e6c61c863eb35a0e67
Harshit <onmailharshit@gmail.com>
2025-06-06 19:30:22 +0530

    another commit

--- abhigya.py
+++ abhigya.py
@@ -2,8 +2,7 @@ import streamlit as st
 import base64
 import time
 from datetime import datetime
-from langchain_community.chat_models import ChatGoogleGenerativeAI
-
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import HumanMessage
 from prompt_utils import build_prompt
 from memory_retriever import retrieve_memories
